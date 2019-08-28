d9bbd7de8c78e8715d0b2774365a482b94953452
Christian Oberdörfer <oberdoerfer@quantum.qa>
2019-08-28 23:20:27 +0200

    Added Android and iOS

---
+++ src/androidMain/kotlin/qa/quantum/qlog/QLogAndroid.kt
@@ -0,0 +1,18 @@
+@file:Suppress("FunctionName")
+
+package qa.quantum.qlog
+
+actual fun <T> QLogHighlight(loggedObject: T) {
+}
+
+actual fun <T> QLogDebug(loggedObject: T) {
+}
+
+actual fun <T> QLogInfo(loggedObject: T) {
+}
+
+actual fun <T> QLogWarning(loggedObject: T) {
+}
+
+actual fun <T> QLogError(loggedObject: T) {
+}
